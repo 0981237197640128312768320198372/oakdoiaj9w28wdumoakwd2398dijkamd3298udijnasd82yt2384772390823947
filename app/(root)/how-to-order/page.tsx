6bd47007b8a2af7706267ada83a740f7b1ba8b60
dokmaistore <dokmaishop22@gmail.com>
2024-10-04 20:27:55 +0700

    983487535394785763248927940327409274892634087401934718947983487535394785763248927940327409274892634087401934718947983487535394785763248927940327409274892634087401934718947983487535394785763248927940327409274892634087401934718947983487535394785763248927940327409274892634087401934718947

--- app/(root)/how-to-order/page.tsx
+++ app/(root)/how-to-order/page.tsx
@@ -4,6 +4,7 @@ import { Timeline } from "@/components/Timeline"
 import Image from "next/image"
 import React from "react"
 import { GiClick } from "react-icons/gi"
+import { BsChatLeftTextFill } from "react-icons/bs"
 import chooseplan from "@/assets/images/chooseplan.png"
 import Link from "next/link"
 
@@ -11,15 +12,42 @@ const page = () => {
   const data = [
     {
       icon: <GiClick className='text-2xl text-light-500' />,
-      title: "Choose Plan",
+      title: "Choosing Plan",
       content: (
         <div>
           <p className='text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8'>
             เลือกแพ็กเกจที่ใช่สำหรับคุณ{" "}
-            <Link href={"/prices"} className='text-primary'>
+            <Link href={"/prices"} target='__blank' className='text-primary'>
               คลิกที่นี่
             </Link>
             เพื่อดูราคา
+          </p>
+          <div className='flex w-full h-full gap-4'>
+            <Image
+              src={chooseplan}
+              alt='Dokmai Store Price List Netflix Premium Plan'
+              width={500}
+              height={500}
+              className='rounded-lg'
+            />
+          </div>
+        </div>
+      ),
+    },
+    {
+      icon: <BsChatLeftTextFill className='text-2xl text-light-500' />,
+      title: "Ordering",
+      content: (
+        <div>
+          <p className='text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8'>
+            แจ้งแพ็กเกจที่ลูกค้าต้องการใน{" "}
+            <Link
+              href={"https://lin.ee/Ovlixv5"}
+              target='__blank'
+              className='text-primary'
+            >
+              Line chat
+            </Link>
           </p>
           <div className='flex w-full h-full gap-4'>
             <Image
