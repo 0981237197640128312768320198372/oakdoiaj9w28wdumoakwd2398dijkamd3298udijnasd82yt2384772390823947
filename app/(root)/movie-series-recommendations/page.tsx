2196bcd5b8825472a8929a260ed77632a8a12084
dokmaistore <dokmaishop22@gmail.com>
2024-10-08 03:14:08 +0700

    19023871863210976317631792853127461357121902387186321097631763179285312746135712190238718632109763176317928531274613571219023871863210976317631792853127461357121902387186321097631763179285312746135712190238718632109763176317928531274613571219023871863210976317631792853127461357121902387186321097631763179285312746135712190238718632109763176317928531274613571219023871863210976317631792853127461357121902387186321097631763179285312746135712190238718632109763176317928531274613571219023871863210976317631792853127461357121902387186321097631763179285312746135712190238718632109763176317928531274613571219023871863210976317631792853127461357121902387186321097631763179285312746135712

--- app/(root)/movie-series-recommendations/page.tsx
+++ app/(root)/movie-series-recommendations/page.tsx
@@ -1,8 +1,8 @@
 import React from "react"
-import { Recommendations } from "@/app/api/GoogleSheetAPI"
+// import { Recommendations } from "@/app/api/GoogleSheetAPI"
 const page = async () => {
-  const recommendationsData = await Recommendations()
-  console.log(recommendationsData)
+  //   const recommendationsData = await Recommendations()
+  //   console.log(recommendationsData)
   return (
     <div className='flex justify-center items-center h-screen w-full text-light-100'>
       Coming soon
