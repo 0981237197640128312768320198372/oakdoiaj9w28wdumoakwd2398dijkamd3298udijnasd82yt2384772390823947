1211a897330ad04e8cac8f396662bfb5f5666640
dokmaistore <dokmaishop22@gmail.com>
2024-10-25 05:40:54 +0700

    1673187318037147084576235130847136130877516731873180371470845762351308471361308775167318731803714708457623513084713613087751673187318037147084576235130847136130877516731873180371470845762351308471361308775167318731803714708457623513084713613087751673187318037147084576235130847136130877516731873180371470845762351308471361308775167318731803714708457623513084713613087751673187318037147084576235130847136130877516731873180371470845762351308471361308775167318731803714708457623513084713613087751673187318037147084576235130847136130877516731873180371470845762351308471361308775167318731803714708457623513084713613087751673187318037147084576235130847136130877516731873180371470845762351308471361308775167318731803714708457623513084713613087751673187318037147084576235130847136130877516731873180371470845762351308471361308775167318731803714708457623513084713613087751673187318037147084576235130847136130877516731873180371470845762351308471361308775167318731803714708457623513084713613087751673187318037147084576235130847136130877516731873180371470845762351308471361308775167318731803714708457623513084713613087751673187318037147084576235130847136130877516731873180371470845762351308471361308775167318731803714708457623513084713613087751673187318037147084576235130847136130877516731873180371470845762351308471361308775167318731803714708457623513084713613087751673187318037147084576235130847136130877516731873180371470845762351308471361308775

--- public/next.config.mjs
+++ public/next.config.mjs
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["drive.google.com", "drive.usercontent.google.com"],
+    domains: ["drive.google.com", "drive.usercontent.google.com", "*"],
   },
 }
-withOffline({
-  workboxOpts: {
-    swDest: "static/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "offlineCache",
-          expiration: {
-            maxEntries: 200,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
-  },
-})
 export default nextConfig
