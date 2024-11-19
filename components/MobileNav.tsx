e019408f97f2eadbbf90983beff59f2c40e46ba4
dokmaistore <dokmaishop22@gmail.com>
2024-11-19 13:07:57 +0700

    1802361371639713108237120836127936123812372183126760987180236137163971310823712083612793612381237218312676098718023613716397131082371208361279361238123721831267609871802361371639713108237120836127936123812372183126760987180236137163971310823712083612793612381237218312676098718023613716397131082371208361279361238123721831267609871802361371639713108237120836127936123812372183126760987180236137163971310823712083612793612381237218312676098718023613716397131082371208361279361238123721831267609871802361371639713108237120836127936123812372183126760987180236137163971310823712083612793612381237218312676098718023613716397131082371208361279361238123721831267609871802361371639713108237120836127936123812372183126760987180236137163971310823712083612793612381237218312676098718023613716397131082371208361279361238123721831267609871802361371639713108237120836127936123812372183126760987180236137163971310823712083612793612381237218312676098718023613716397131082371208361279361238123721831267609871802361371639713108237120836127936123812372183126760987180236137163971310823712083612793612381237218312676098718023613716397131082371208361279361238123721831267609871802361371639713108237120836127936123812372183126760987180236137163971310823712083612793612381237218312676098718023613716397131082371208361279361238123721831267609871802361371639713108237120836127936123812372183126760987

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -11,16 +11,16 @@ const MobileNav = () => {
   const path = usePathname()
   return (
     <Drawer>
-      <DrawerTrigger className='xl:hidden dark:hover:bg-white/10 hover:bg-black/5 p-2 focus:outline-none focus:ring-0 dark:focus:bg-gray-800'>
+      <DrawerTrigger className='xl:hidden dark:hover:bg-white/10 hover:bg-black/5 p-2 focus:outline-none focus:ring-0 dark:focus:bg-dark-700'>
         <CiMenuBurger className='duration-500 text-dark-800 dark:text-primary text-3xl' />
       </DrawerTrigger>
-      <DrawerContent className='bg-gray-200 dark:bg-dark-600 border-none px-5 gap-10 focus:outline-none focus:ring-0'>
+      <DrawerContent className='bg-light-200 dark:bg-dark-600 border-none px-5 gap-10 focus:outline-none focus:ring-0'>
         <div className='grid grid-cols-2 w-full h-fit gap-1'>
           {navButtons.map((nav, i) => (
             <Link
               href={nav.url}
               key={i}
-              className={`dark:text-gray-400 w-full flex justify-start items-start h-12 md:text-sm font-medium group duration-200 p-2 ${
+              className={`dark:text-light-400 w-full flex justify-start items-start h-12 md:text-sm font-medium group duration-200 p-2 ${
                 path === nav.url
                   ? "font-black transition-transform duration-700 "
                   : ""
