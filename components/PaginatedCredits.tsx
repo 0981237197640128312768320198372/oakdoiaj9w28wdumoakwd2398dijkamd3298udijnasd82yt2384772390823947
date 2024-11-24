3d20b2ade6889e359e1e6a1ac65a8bdf2d222198
dokmaistore <dokmaishop22@gmail.com>
2024-11-24 18:34:14 +0700

    208136017418923469208136017418923469208136017418923469208136017418923469208136017418923469208136017418923469208136017418923469208136017418923469208136017418923469208136017418923469

--- components/PaginatedCredits.tsx
+++ components/PaginatedCredits.tsx
@@ -146,7 +146,7 @@ export default function PaginatedCredits() {
   )
 
   return (
-    <div className='flex flex-col justify-center w-full h-full items-center px-5 lg:px-0'>
+    <div className='flex flex-col justify-center w-full h-full items-center'>
       <div className='w-fit h-full grid grid-cols-2 lg:grid-cols-3 gap-5 pb-10'>
         {loading
           ? Array.from({ length: limit }).map((_, index) => (
