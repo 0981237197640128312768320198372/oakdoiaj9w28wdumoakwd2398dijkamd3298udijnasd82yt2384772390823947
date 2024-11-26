610a9f11d76f4c5af641be284fd2742e4d3a2db6
dokmaistore <dokmaishop22@gmail.com>
2024-11-27 00:55:43 +0700

    9073817238091267389137019823

--- components/ShowPremiumApps.tsx
+++ components/ShowPremiumApps.tsx
@@ -16,6 +16,7 @@ import Link from "next/link"
 import Loading from "@/components/Loading"
 import EmailList from "./EmailList"
 import CopyToClipboard from "./CopyToClipboard"
+import { logActivity } from "@/lib/utils"
 
 export const ShowPremiumApps = () => {
   const [inputPersonalKey, setInputPersonalKey] = useState<string>("")
@@ -125,29 +126,6 @@ export const ShowPremiumApps = () => {
     setValidatingPersonalKey(false)
   }
 
-  async function logActivity(type: string, user: string, details: any) {
-    const logEntry = {
-      type, // Type of activity (e.g., "Checkout", "Login", "Top-Up")
-      user, // Personal key or identifier of the user
-      details, // Additional structured data related to the activity
-    }
-    // console.log("API KEY", process.env.NEXT_PUBLIC_LOGGING_API_KEY)
-    const url = process.env.NEXT_PUBLIC_API_URL
-    const response = await fetch(`${url}/api/log_activity`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "x-api-key": process.env.NEXT_PUBLIC_LOGGING_API_KEY || "",
-      },
-      body: JSON.stringify({ logEntry }),
-    })
-
-    if (!response.ok) {
-      console.error("Failed to log activity")
-    } else {
-      console.log("Activity logged successfully")
-    }
-  }
   const validatePersonalKey = async (key: string) => {
     try {
       setError(null)
@@ -162,7 +140,6 @@ export const ShowPremiumApps = () => {
           description: "Logged in successfully",
         })
         const userInfoData = await userInfoRes.json()
-        console.log(userInfoData)
         setUserInfo(userInfoData.data)
         localStorage.setItem("personalKey", key)
         fetchPremiumData(key)
