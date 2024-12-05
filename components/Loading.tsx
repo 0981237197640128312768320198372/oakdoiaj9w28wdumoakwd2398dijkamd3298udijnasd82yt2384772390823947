41c78cad0ae9ded3538333315b5ce14755f628e0
dokmaistore <dokmaishop22@gmail.com>
2024-12-06 01:14:32 +0700

    01827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715018273912638910371501827391263891037150182739126389103715

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -5,15 +5,15 @@ import dokmaicoin from "@/assets/images/dokmaicoin.gif"
 const LoadingAnimation = ({ text }: { text?: string }) => {
   return (
     <div className='fixed top-0 left-0 z-[100] w-screen h-screen flex flex-col justify-center items-center bg-dark-800'>
-      <div className='relative flex flex-col items-center justify-center gap-3 min-w-32 min-h-32 w-fit h-fit'>
+      <div className='relative flex flex-col items-center justify-center gap-3'>
         {/* <div className='w-20 h-20 border-y-[1px] border-y-primary/30 border-x-2 border-x-primary rounded-full animate-spin'></div> */}
         <Image
           src={dokmaicoin}
           alt='Loading Logo | Dokmai Store'
-          width={200}
-          height={200}
+          width={100}
+          height={100}
           loading='lazy'
-          className=''
+          className='w-32 h-auto'
         />
       </div>
       <p className='mt-2'>{text}</p>
