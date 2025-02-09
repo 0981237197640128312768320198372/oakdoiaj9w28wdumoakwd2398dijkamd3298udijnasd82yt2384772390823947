c1604599d0dcb4236e476ab366d93f47663938d7
0981237197640128312768320198372 <dokmaishop22@gmail.com>
2025-02-09 23:14:43 +0700

    2141523424324

--- app/api/get_netflix_top10/route.ts
+++ app/api/get_netflix_top10/route.ts
@@ -1,52 +1,40 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { NextResponse } from "next/server"
+import { NextResponse } from 'next/server';
 
 interface DataItem {
-  id: string
-  showId: string
-  category?: string
-  rank?: string
-  showName?: string
-  seasonName?: string
-  horizontal?: string
+  id: string;
+  showId: string;
+  category?: string;
+  rank?: string;
+  showName?: string;
+  seasonName?: string;
+  horizontal?: string;
 }
 
 export async function GET() {
   try {
     const [dataRes, namesRes, boxartRes] = await Promise.all([
-      fetch(
-        "https://www.netflix.com/tudum/top10/data/weeks/20241201-data.json"
-      ),
-      fetch(
-        "https://www.netflix.com/tudum/top10/data/weeks/20241201-en-names.json"
-      ),
-      fetch(
-        "https://www.netflix.com/tudum/top10/data/weeks/20241201-th-boxart.json"
-      ),
-    ])
+      fetch('https://www.netflix.com/tudum/top10/data/weeks/20250126-data.json'),
+      fetch('https://www.netflix.com/tudum/top10/data/weeks/20250126-en-names.json'),
+      fetch('https://www.netflix.com/tudum/top10/data/weeks/20250126-th-boxart.json'),
+    ]);
 
-    const dataJson = await dataRes.json()
-    const namesJson = await namesRes.json()
-    const boxartJson = await boxartRes.json()
+    const dataJson = await dataRes.json();
+    const namesJson = await namesRes.json();
+    const boxartJson = await boxartRes.json();
 
     const thailandData =
-      dataJson.countries.find(
-        (countryEntry: any) => countryEntry[0] === "TH"
-      )?.[1] || []
+      dataJson.countries.find((countryEntry: any) => countryEntry[0] === 'TH')?.[1] || [];
 
     const dataItems = thailandData.map((item: any) => ({
       id: item.id,
       showId: item.showId,
       category: item.category,
       rank: item.rank,
-    }))
-    const namesMap = Object.fromEntries(
-      namesJson.map((item: any) => [item.id, item])
-    )
-    const boxartMap = Object.fromEntries(
-      boxartJson.map((item: any) => [item.id, item])
-    )
+    }));
+    const namesMap = Object.fromEntries(namesJson.map((item: any) => [item.id, item]));
+    const boxartMap = Object.fromEntries(boxartJson.map((item: any) => [item.id, item]));
 
     const mergedData: DataItem[] = dataItems.map((item: DataItem) => ({
       ...item,
@@ -58,14 +46,11 @@ export async function GET() {
           : undefined,
       horizontal: boxartMap[item.id]?.horizontal,
       vertical: boxartMap[item.id]?.vertical,
-    }))
+    }));
 
-    return NextResponse.json(mergedData)
+    return NextResponse.json(mergedData);
   } catch (error) {
-    console.error("Failed to fetch or process data:", error)
-    return NextResponse.json(
-      { error: "Failed to fetch or process data" },
-      { status: 500 }
-    )
+    console.error('Failed to fetch or process data:', error);
+    return NextResponse.json({ error: 'Failed to fetch or process data' }, { status: 500 });
   }
 }
