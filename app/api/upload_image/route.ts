1e9ed99339f0860c7ed5d52e44700c63b864251c
81782641082461074508273912378186470 <admin@autolaku.com>
2025-03-22 19:22:47 +0700

    53243242

--- app/api/upload_image/route.ts
+++ app/api/upload_image/route.ts
@@ -1,26 +1,26 @@
-// import mongoose from 'mongoose';
-// import { GridFSBucket } from 'mongodb';
+import mongoose from 'mongoose';
+import { GridFSBucket } from 'mongodb';
 
-// interface DatabaseConnection {
-//   gridFSBucket: GridFSBucket;
-// }
+interface DatabaseConnection {
+  gridFSBucket: GridFSBucket;
+}
 
-// let cachedConnection: DatabaseConnection | null = null;
+let cachedConnection: DatabaseConnection | null = null;
 
-// export async function connectToDatabase(): Promise<DatabaseConnection> {
-//   if (cachedConnection) {
-//     return cachedConnection;
-//   }
+export async function connectToDatabase(): Promise<DatabaseConnection> {
+  if (cachedConnection) {
+    return cachedConnection;
+  }
 
-//   const MONGODB_URI = process.env.MONGODB_URI;
-//   if (!MONGODB_URI) {
-//     throw new Error('MONGODB_URI is not defined in environment variables');
-//   }
+  const MONGODB_URI = process.env.MONGODB_URI;
+  if (!MONGODB_URI) {
+    throw new Error('MONGODB_URI is not defined in environment variables');
+  }
 
-//   await mongoose.connect(MONGODB_URI);
-//   const db = mongoose.connection.db;
-//   const gridFSBucket = new GridFSBucket(db, { bucketName: 'images' });
+  await mongoose.connect(MONGODB_URI);
+  const db = mongoose.connection.db;
+  const gridFSBucket = new GridFSBucket(db, { bucketName: 'images' });
 
-//   cachedConnection = { gridFSBucket };
-//   return cachedConnection;
-// }
+  cachedConnection = { gridFSBucket };
+  return cachedConnection;
+}
