27e936e15ea473be05829fbeb266327ed330fa29
81782641082461074508273912378186470 <admin@autolaku.com>
2025-03-23 21:30:33 +0700

    5234341513

--- next.config.mjs
+++ next.config.mjs
@@ -3,34 +3,34 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "drive.google.com",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "drive.usercontent.google.com",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'drive.usercontent.google.com',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "dnm.nflximg.net",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'dnm.nflximg.net',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "www.netflix.com",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'www.netflix.com',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "cdn.dokmaistore.com",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'cdn.dokmaistore.com',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "storage.googleapis.com",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '**',
       },
     ],
   },
@@ -38,17 +38,22 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/deposit",
-        destination: "https://lin.ee/Ovlixv5",
+        source: '/deposit',
+        destination: 'https://lin.ee/Ovlixv5',
         permanent: false,
       },
       {
-        source: "/register",
-        destination: "https://lin.ee/Ovlixv5",
+        source: '/register',
+        destination: 'https://lin.ee/Ovlixv5',
         permanent: false,
       },
-    ]
+      {
+        source: '/indefatigability/perspicacity/thebot',
+        destination: 'https://www.mediafire.com/folder/dy6nfl2n96w9a/Thebot',
+        permanent: false,
+      },
+    ];
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
