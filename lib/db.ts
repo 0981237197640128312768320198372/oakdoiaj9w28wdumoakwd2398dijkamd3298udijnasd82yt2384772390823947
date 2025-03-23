42bf9647b5f842f80687d9693a62b325cc9b10eb
81782641082461074508273912378186470 <admin@autolaku.com>
2025-03-23 16:39:42 +0700

    235523432

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,12 @@
 import mongoose from 'mongoose';
-import { GridFSBucket } from 'mongodb'; // Import from Mongoose's mongodb
+import { GridFSBucket } from 'mongodb';
 
-// Ensure MONGODB_URI is defined
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
 }
 
-// Define the return type for connectToDatabase
 interface DatabaseConnection {
   connection: mongoose.Connection;
   gridFSBucket: GridFSBucket;
@@ -22,11 +20,9 @@ export async function connectToDatabase(): Promise<DatabaseConnection> {
     return { connection: cachedConnection, gridFSBucket };
   }
 
-  // Connect to MongoDB
   const connection = await mongoose.connect(MONGODB_URI);
   cachedConnection = connection.connection;
 
-  // Ensure the connection is established and db is defined
   const db = cachedConnection.db;
   if (!db) {
     throw new Error('Failed to connect to database: db is undefined');
