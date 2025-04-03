30139c40362dcd1af3bd2bb5c9c1361041f947cc
81782641082461074508273912378186470 <admin@autolaku.com>
2025-04-03 14:58:42 +0700

    5234325234

--- models/TheBot.ts
+++ models/TheBot.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 // Define the schema for bot updates
 const TheBotSchema = new mongoose.Schema({
   timestamp: { type: String, required: true },
-  type: { type: String, required: true, enum: ['error', 'success', 'status'] },
+  type: { type: String, required: true, enum: ['error', 'success', 'status', 'info'] },
   message: { type: String, required: true },
   details: {
     email: { type: String },
