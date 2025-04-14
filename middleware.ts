2916f7966328e8a96f5f34359102b8fbbbc2897c
81782641082461074508273912378186470 <admin@autolaku.com>
2025-04-14 11:30:12 +0700

    53242353534534

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(req: NextRequest) {
+interface GeoRequest extends NextRequest {
+  geo?: { country?: string };
+}
+
+export async function middleware(req: GeoRequest) {
   const userAgent = req.headers.get('user-agent') || '';
   const allowedUserAgent =
     'Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6666.666 Safari/537.36';
@@ -14,20 +17,22 @@ export async function middleware(req: NextRequest) {
   const acceptLanguage = req.headers.get('accept-language') || '';
   const browserLanguage = acceptLanguage.split(',')[0];
   const isRestrictedLanguage = browserLanguage.startsWith('id');
-
-  const userCountry = (req as any).geo?.country || '';
+  const userCountry = req.geo?.country || '';
   const isRestrictedCountry = userCountry === 'ID';
 
+  // Consider removing or refining this block if not required
   if (isRestrictedLanguage || isRestrictedCountry) {
-    return new Response(null, {
-      status: 502,
-      headers: { 'Content-Type': 'text/html' },
-    });
+    return new Response(null, { status: 403, headers: { 'Content-Type': 'text/html' } });
   }
+
   const hostname = req.headers.get('host')?.toLowerCase();
   const path = req.nextUrl.pathname;
 
-  if (hostname === 'admin.dokmaistore.com' || hostname?.includes('localhost')) {
+  if (
+    hostname === 'admin.dokmaistore.com' ||
+    hostname === 'localhost' ||
+    hostname?.startsWith('localhost:')
+  ) {
     return NextResponse.rewrite(new URL(`/admin${path}`, req.url));
   }
 
