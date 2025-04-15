9ed0f6e6ba5f09508e2f51caf1c1a51e99dd7f02
81782641082461074508273912378186470 <admin@autolaku.com>
2025-04-15 16:15:08 +0700

    5234234

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -12,7 +12,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href="/admin-manifest.json" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className="font-aktivGroteskRegular min-h-screen bg-dark-800 text-light-200 overflow-x-hidden selection:bg-primary/10 selection:text-primary flex flex-col justify-start w-full items-center p-5 xl:p-10">
