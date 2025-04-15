3172614e9465cd3abd1642ab0f018d4b1fbabf97
81782641082461074508273912378186470 <admin@autolaku.com>
2025-04-15 16:07:20 +0700

    5324325234

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,22 +1,23 @@
 /* eslint-disable @next/next/next-script-for-ga */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import Authenticator from "@/components/Authenticator"
-import "@/styles/globals.css"
-import Image from "next/image"
+import Authenticator from '@/components/Authenticator';
+import '@/styles/globals.css';
+import Image from 'next/image';
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <link rel="manifest" href="/admin-manifest.json" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className='font-aktivGroteskRegular min-h-screen bg-dark-800 text-light-200 overflow-x-hidden selection:bg-primary/10 selection:text-primary flex flex-col justify-start w-full items-center p-5 xl:p-10'>
+      <body className="font-aktivGroteskRegular min-h-screen bg-dark-800 text-light-200 overflow-x-hidden selection:bg-primary/10 selection:text-primary flex flex-col justify-start w-full items-center p-5 xl:p-10">
         <Authenticator>{children}</Authenticator>
       </body>
     </html>
-  )
+  );
 }
