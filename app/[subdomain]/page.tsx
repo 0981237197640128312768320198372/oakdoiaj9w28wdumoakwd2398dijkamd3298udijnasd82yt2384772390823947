4172753990166da34d30e6725cbe889e79e38b8d
81782641082461074508273912378186470 <admin@autolaku.com>
2025-05-21 11:13:23 +0700

    5234326234

--- app/[subdomain]/page.tsx
+++ app/[subdomain]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { fetchStoreData } from '@/lib/fetchStoreData';
 import { cn } from '@/lib/utils';
 
@@ -5,12 +6,37 @@ interface StorePageProps {
   params: { subdomain: string };
 }
 
+export async function generateMetadata({ params }: StorePageProps) {
+  const { subdomain } = params;
+  try {
+    const { seller } = await fetchStoreData(subdomain);
+    const storeName = seller?.store.name || 'Dokmai Store';
+    const storeDescription = seller?.store.description || 'A digital product marketplace';
+    return {
+      title: storeName,
+      description: storeDescription,
+      openGraph: {
+        title: storeName,
+        description: storeDescription,
+        url: `https://${seller?.username}.dokmai.store/`,
+      },
+    };
+  } catch (error) {
+    console.error('Error generating metadata:', error);
+    return {
+      title: 'Dokmai Store',
+      description: 'A digital product marketplace',
+    };
+  }
+}
+
+// Page component
 export default async function StorePage({ params }: StorePageProps) {
   const { subdomain } = params;
-  console.log(subdomain);
   const { theme, seller } = await fetchStoreData(subdomain);
 
   const storeName = seller?.store.name;
+  const storeDescription = seller?.store.description;
   const primaryColor = theme?.primaryColor || 'inherit';
 
   return (
