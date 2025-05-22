c0a06534d3b7c35e5ce127d250050ab31582589a
81782641082461074508273912378186470 <admin@autolaku.com>
2025-05-22 12:45:08 +0700

    634342624

--- components/seller/public/StoreNavbar.tsx
+++ components/seller/public/StoreNavbar.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import dokmaiwithtext from '@/assets/images/dokmaiwithtext.png';
@@ -15,21 +14,19 @@ interface StoreNavbarProps {
 }
 
 export const StoreNavbar: React.FC<StoreNavbarProps> = ({ seller }) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
   return (
     <nav className="fixed flex flex-col px-5 xl:p-0 items-center justify-center top-0 left-0 w-full transition-transform duration-500 z-50 transform">
       <div className="w-full mt-5 gap-10 bg-dark-700 flex py-1 px-2 lg:p-2 rounded-full max-w-screen-lg justify-between duration-1000 items-center border-[1px] border-dark-500">
         <div className="flex items-center gap-1 w-fit select-none group transition-all duration-500">
           {seller ? (
             <>
-              <div className="relative w-10 h-10 overflow-hidden rounded-full border-dark-400 border-[1px]">
+              <div className="relative w-10 h-10 border-dark-400 border-[1px] overflow-hidden rounded-full bg-cover bg-center">
                 <Image
                   src={seller.store.logoUrl || dokmailogosquare}
                   alt={seller.store.name}
-                  layout="fill"
-                  objectFit="cover"
-                  className="border-[1px] border-dark-600"
+                  width={100}
+                  height={100}
+                  className="border-[1px] border-dark-600 w-full h-full overflow-hidden rounded-full"
                 />
               </div>
             </>
