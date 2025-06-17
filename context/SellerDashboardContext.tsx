a2bb083687d1043cf519eafed99b3e5ba1ead01d
81782641082461074508273912378186470 <admin@autolaku.com>
2025-06-17 08:55:12 +0700

    6345437345345

--- context/SellerDashboardContext.tsx
+++ context/SellerDashboardContext.tsx
@@ -1,15 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
 import { consoleFuck } from '@/lib/utils';
 import React, { createContext, useContext, useState, ReactNode } from 'react';
-
-// Define the available views in the seller dashboard
-export type DashboardView = 'overview' | 'products' | 'orders' | 'analytics' | 'edit-profile';
-
-// Define the context type
 interface SellerDashboardContextType {
-  activeView: DashboardView;
-  setActiveView: (view: DashboardView) => void;
+  activeView: any;
+  setActiveView: (view: any) => void;
 }
 
 // Create the context with default values
@@ -20,7 +16,7 @@ const SellerDashboardContext = createContext<SellerDashboardContextType>({
 
 // Create a provider component
 export const SellerDashboardProvider = ({ children }: { children: ReactNode }) => {
-  const [activeView, setActiveView] = useState<DashboardView>('overview');
+  const [activeView, setActiveView] = useState('overview');
   console.log(
     `%c${consoleFuck}`,
     'display: inline-block; width: 64px; height: 64px; text-align: center; line-height: 64px; font-size: 32px; color: white; background: #0f0f0f; padding: 10px; font-weight: bold; border-radius: 10px;'
